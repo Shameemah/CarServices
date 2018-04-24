d6ded02d8e8f3526de7dccde8a5430449ca82463
Shameemah <Shameemah@shameemah.local>
2018-04-24 11:02:44 -0500

    Implemented use of web service for finding parts by ID

--- src/main/java/edu/iit/sat/itmd4515/sfuseini/ejb/PartsService.java
+++ src/main/java/edu/iit/sat/itmd4515/sfuseini/ejb/PartsService.java
@@ -6,9 +6,19 @@
 package edu.iit.sat.itmd4515.sfuseini.ejb;
 
 import edu.iit.sat.itmd4515.sfuseini.domain.Parts;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
 import javax.inject.Named;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 /**
  *
@@ -31,10 +41,38 @@ public class PartsService extends BaseService<Parts> {
      * @return
      */
     public Parts findByPartId(int serialNumber) {
-        return getEntityManager()
-                .createNamedQuery("Parts.findByPartsId", Parts.class)
-                .setParameter("serialNumber", serialNumber)
-                .getSingleResult();
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        Parts p = new Parts();
+        
+         try {
+            HttpGet getRequest = new HttpGet("http://localhost:8080/CarServices/webresources/parts/" + serialNumber);
+            getRequest.addHeader("accept", "application/json");
+            HttpResponse httpResponse = httpclient.execute(getRequest);
+            HttpEntity entity = httpResponse.getEntity();
+            DateFormat df = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+            BufferedReader br = new BufferedReader(
+                         new InputStreamReader((entity.getContent())));
+            String output = br.readLine();
+            String[] ar=output.split(",");
+            
+            Long serialNum = Long.parseLong(ar[0].substring(ar[0].lastIndexOf("=") + 1));
+            String type = ar[1].substring(ar[1].lastIndexOf("=") + 1);
+            String category = ar[2].substring(ar[2].lastIndexOf("=") + 1);
+            String brandName = ar[3].substring(ar[3].lastIndexOf("=") + 1);
+            String model = ar[4].substring(ar[4].lastIndexOf("=") + 1);
+            Integer registrationNum = Integer.parseInt(ar[5].substring(ar[5].lastIndexOf("=") + 1));
+            Integer engineNum = Integer.parseInt(ar[6].substring(ar[6].lastIndexOf("=") + 1));
+            Integer chassisNum = Integer.parseInt(ar[7].substring(ar[7].lastIndexOf("=") + 1));
+            Double odometerReading = Double.parseDouble(ar[8].substring(ar[8].lastIndexOf("=") + 1));
+            Date manufactureDate = df.parse(ar[9].substring(ar[9].lastIndexOf("=") + 1));
+            Date purchaseDate = df.parse(ar[10].substring(ar[10].lastIndexOf("=") + 1));
+
+            p = new Parts(serialNum, type, category, brandName, model, registrationNum, engineNum, chassisNum, odometerReading, manufactureDate, purchaseDate);
+
+            } catch (Exception e) {
+            System.out.println(e);
+        }
+            return p;
     }
 
     /**
